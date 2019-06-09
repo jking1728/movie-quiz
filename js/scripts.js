6220ed3425086777ed3002a8fe66c1bead05b4d3
Jordan King <supapeachx@gmail.com>
2019-06-09 16:35:07 -0700

    updated functionality of quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
-//back end
-
-
-// front end
 $(document).ready(function() {
-    $("#form-submit").submit(function(event) {
-        debugger;
-        console.log(event);
+    $("form#quiz").submit(function(event) {
         var answer1 = parseInt($("input:radio[name=question1]:checked").val());
         var answer2 = parseInt($("input:radio[name=question2]:checked").val());
         var answer3 = parseInt($("input:radio[name=question3]:checked").val());
         var answer4 = parseInt($("input:radio[name=question4]:checked").val());
         var answer5 = parseInt($("input:radio[name=question5]:checked").val());
         var totalScore = (answer1 + answer2 + answer3 + answer4 + answer5)
-        $(".Iron-Man").show();
-        if (totalScore <= 5) {
+        
+        if ((answer1 + answer2 + answer3 + answer4 + answer5) <= 5) {
             $(".black-panther").show();
         } else if (totalScore > 5 && totalScore <= 10) {
             $(".Iron-Man").show();
@@ -24,6 +18,13 @@ $(document).ready(function() {
         } else {
             $(".Spider-Man").show();
         }
+        $("form#quiz").hide();
+        $("button#reset").show();
+        $("button#reset").click(function() {
+            $("form#quiz").show();
+            $(".heroes").toggle();
+            $("button#reset").hide();
+        })
         event.preventDefault();
         
     })
