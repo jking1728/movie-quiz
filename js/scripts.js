b3694855e23e4a1f9a2118a6ceb5575fedc86b20
Jordan King <supapeachx@gmail.com>
2019-06-09 18:03:37 -0700

    completed quiz and readme

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,27 @@ $(document).ready(function() {
         var answer3 = parseInt($("input:radio[name=question3]:checked").val());
         var answer4 = parseInt($("input:radio[name=question4]:checked").val());
         var answer5 = parseInt($("input:radio[name=question5]:checked").val());
-        var totalScore = (answer1 + answer2 + answer3 + answer4 + answer5)
+        var totalScore = (answer1 + answer2 + answer3 + answer4 + answer5);
         
-        if ((answer1 + answer2 + answer3 + answer4 + answer5) <= 5) {
-            $(".black-panther").show();
+        if (totalScore <= 5) {
+            $("#black-panther").show();
         } else if (totalScore > 5 && totalScore <= 10) {
-            $(".Iron-Man").show();
+            $("#Iron-Man").show();
         } else if (totalScore > 10 && totalScore <= 15) {
-            $(".Scarlet-Witch").show();
+            $("#Scarlet-Witch").show();
         } else if (totalScore > 15 && totalScore < 21) {
-            $(".Captain-America").show();
+            $("#Captain-America").show();
         } else {
-            $(".Spider-Man").show();
-        }
+            $("#Spider-Man").show();
+        };
         $("form#quiz").hide();
         $("button#reset").show();
         $("button#reset").click(function() {
             $("form#quiz").show();
-            $(".heroes").toggle();
+            $(".heroes").hide();
             $("button#reset").hide();
-        })
+        });
         event.preventDefault();
         
-    })
-})
+    });
+});
---
+++ readme.md
@@ -0,0 +1,34 @@
+# _Marvel Quiz_
+
+#### _JavaScript and jQuery Independent Project, 06.09.2019_
+
+#### By _**Jordan King**_
+
+## Description
+
+_This webpage asks the user 5 questions to determine what Marvel Avengers character they are most like. After going though the quiz you can reset to try again. This project is an independent project to show knowledge of JavaScript, jQuery, and using branching to return different results_
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Open index.html in your choice of web browser_
+
+_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+
+## Support and contact details
+
+_email:supapeachx@gmail.com_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _JavaScript_
+* _jQuery_
+
+### License
+
+*This software is licensed under the MIT license*
+
+Copyright (c) 2015 **_Jordan King_**
